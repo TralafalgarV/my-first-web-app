1c6c5db38cf6bd9679ef8727760df15b94dd3151
TralafalgarV <578895238@qq.com>
2018-01-06 17:20:13 +0800

    修复bug，fetchImg可以返回nginx允许访问的img路径

--- App/Server/routes/article.js
+++ App/Server/routes/article.js
@@ -120,7 +120,7 @@ router.post('/fetchImg', function (req, res) {
         // 返回给前端图片信息
         var imgInfo = {
             name: files.imgData.name,
-            path: API + files.imgData.name
+            path: API + "/" + fields.token + files.imgData.name // 图片在服务器的访问IP
         }
         res.send(imgInfo)
         // // 图片路径更新到数据库
